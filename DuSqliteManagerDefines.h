c91aac41eac3bc4ceed1222269c911996784beec
Bnaimy <36247291+Bnaimy@users.noreply.github.com>
2018-06-04 13:47:36 +0000

    final

---
+++ DuSqliteManagerDefines.h
@@ -0,0 +1,14 @@
+/**
+ * @author Carlos Enrique Duarte Ortiz <carlosduarte.1@hotmail.com>
+*/
+
+#ifndef DUDEFINES_H
+#define DUDEFINES_H
+
+#define APPNAME "DuSQLiteManager"
+#define APPVER "1.0"
+#define ORGDOM "https://www.github.com/Bnaimy/cpp.git"
+#define ORGNAME "GI"
+
+#endif // DUDEFINES_H
+
